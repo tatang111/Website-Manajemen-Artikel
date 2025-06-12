b7ac4df5a6cea45120587d5080543805cb9108ba
tatang <lintangpsa@gmail.com>
2025-06-12 20:22:56 +0700

    add redirect from /

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
+"use client"
+
 import { Input } from "@/components/ui/input";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    redirect("/register")
+  }, [])
+
   return (
     <div className="">
         page
